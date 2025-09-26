ad7efeea8e12fce52b8554c50a7dbfe9bfaf4b1e
Arnaudtrzzo <Rakotoneraa@gmail.com>
2025-09-26 16:43:50 +0300

    aefzfa

--- backend/ams/settings.py
+++ backend/ams/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ams",
         "USER": "postgres",
-        "PASSWORD": "12345678",
+        "PASSWORD": "azerty123",
         "HOST": "localhost",
         "PORT": "5432",
     }
